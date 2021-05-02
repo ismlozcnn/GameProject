757e6cae149cb3e7d9357231462c8c4de6d2693d
İsmail Özcan <l5215067@std.yildiz.edu.tr>
2021-05-02 23:31:45 +0300

    2. hafta ödev

--- src/gameProject/SaleService.java
+++
@@ -1,9 +0,0 @@
-package gameProject;
-
-public interface SaleService {
-	void sell(Player player,Game game);
-	void refund(Player player,Game game);
-	void campaingWithSell(Player player,Game game,Campaign campaign);
-	
-
-}
